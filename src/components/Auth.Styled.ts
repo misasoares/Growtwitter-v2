8b9c0c8e6b6d8921581d1c5efd6591e61f47e163
misael soares <misasoaresms2@gmail.com>
2023-12-23 19:10:46 -0300

    service + redux + create users + getUsers

--- src/components/Auth.Styled.ts
+++ src/components/Auth.Styled.ts
@@ -1,29 +1,42 @@
 import styled from "styled-components";
 
 export const BodyAuth = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    left: 50%;
-`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  left: 50%;
+  background-color: #f1f1f1;
+`;
 
 export const ContainerAuth = styled.div`
-    display: flex;
-    height: 250px;
-    justify-content: center;
-`
+  display: flex;
+  height: 450px;
+  justify-content: center;
+`;
 
 export const SectionStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 40%;
-    border: solid black 1px;
-    justify-content: center;
-`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 20px;
+  background-color: #1d9bf0;
+  color: white;
+`;
+export const SectionStyledTwo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  background-color: #ffffff;
+`;
 export const SignStyledForm = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
--- src/store/modules/users/usersSlice.ts
+++ src/store/modules/users/usersSlice.ts
@@ -1,28 +1,74 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { createUser, createUserDTO, getAllUsers } from "../../../config/services/user.service";
 
 export interface UsersType {
-  id?: string;
-  nome: string;
+  id: string;
+  name: string;
   email: string;
   password: string;
-  idade: number;
-  tipo: string;
+  username: string;
+  iconePerfil: string;
 }
 
-
 const initialState: UsersType[] = [];
+
+export const getUsers = createAsyncThunk("users/getUsers", async () => {
+  try {
+    const response = await getAllUsers();
+
+    const { data } = response;
+
+    return data;
+  } catch (error) {
+    console.error("Erro ao criar usuários:", error);
+    return null;
+  }
+});
+
+export const createNewUser = createAsyncThunk("users/createNewUser", async (dataUser: createUserDTO) => {
+  try {
+    const response = await createUser(dataUser);
+
+    const { data } = response;
+    console.log(data);
+
+    // return {
+    //   id: data.id,
+    //   name: data.name,
+    //   email: data.email,
+    //   password: data.password,
+    //   username: data.username,
+    //   iconePerfil: data.iconePerfil,
+    // };
+  } catch (error) {
+    console.error("Erro ao criar usuários:", error);
+    return null;
+  }
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    createNewUser(state, action: PayloadAction<UsersType[]>) {
+    updateArrayUsers(state, action: PayloadAction<UsersType[]>) {
       state = action.payload;
       return state;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        return state;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        if (action.payload) {
+          state = action.payload;
+        }
+        return state;
+      });
+  },
 });
 
-export const { createNewUser } = usersSlice.actions;
+export const { updateArrayUsers } = usersSlice.actions;
 export default usersSlice.reducer;
