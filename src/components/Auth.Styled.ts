59aeb231515330b5abfe6ca52312f7f9daaa64e3
misael soares <misasoaresms2@gmail.com>
2023-12-23 12:04:54 -0300

    login form start

---
+++ src/components/Auth.Styled.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const BodyAuth = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    left: 50%;
+`
+
+export const ContainerAuth = styled.div`
+    display: flex;
+    height: 250px;
+    justify-content: center;
+`
+
+export const SectionStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    border: solid black 1px;
+    justify-content: center;
+`
+export const SignStyledForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,15 +1,31 @@
-import { Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
+import { BodyAuth, ContainerAuth, SectionStyled, SignStyledForm } from "../components/Auth.Styled";
 
 
 export default function Auth() {
 
   return (
-    <div>
-      <Typography variant="h2" component='h1'>Auth</Typography>
+    <BodyAuth>
+      <ContainerAuth>
 
+        <SectionStyled>
+          <Typography variant="h4" component='h2'>Growtwitter</Typography>
+          <Typography>O Growtwitter é a plataforma definitiva para todos os apaixonados
+            por redes sociais que buscam uma experiência familiar e poderosa, semelhante ao Twitter,
+            comunidade que valoriza a liberdade de expressão, a conexão com pessoas de todo
+            o mundo e a disseminação de ideias.
+          </Typography>
+        </SectionStyled>
+        <SectionStyled>
+          <SignStyledForm>
+            <Typography variant="h4" component='h2'>Entrar no Growtwitter</Typography>
+            <TextField label="Username" />
+            <TextField label="Password" type="password" />
+            <Button variant="contained">Entrar</Button>
+          </SignStyledForm>
+        </SectionStyled>
 
-    
-
-    </div>
+      </ContainerAuth>
+    </BodyAuth>
   );
 }
