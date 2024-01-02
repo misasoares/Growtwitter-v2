f0db6b06ed074287e8070a2c2617326d4df65416
misael soares <misasoaresms2@gmail.com>
2024-01-02 13:48:34 -0300

    add botoes de cada componente

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,10 +8,16 @@ interface NavProps {
 export default function Navbar({ handleLogout }: NavProps) {
   return (
     <NavContainer>
+      <div>Aqui vai o ícone da growdev</div>
+      <div>Página inicial</div>
+      <div>Explorar</div>
+      <div>Perfil</div>
+      <Button variant="contained">Tweetar</Button>
+
+      <div>Aqui vai o ícone do usuário logado - posicionar ao fim da tela</div>
       <Button onClick={handleLogout} variant="contained">
         Desconectar
       </Button>
-      <div>Aqui vai o ícone do usuário logado - posicionar ao fim da tela</div>
     </NavContainer>
   );
 }
