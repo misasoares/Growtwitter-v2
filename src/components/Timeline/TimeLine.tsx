8afbeb7d1d9877b1796a48b57edb7bd3f55c1d8c
misael soares <misasoaresms2@gmail.com>
2024-01-10 21:02:53 -0300

    style:tira o overflow e adiciona svgs

--- src/components/Timeline/TimeLine.tsx
+++ src/components/Timeline/TimeLine.tsx
@@ -1,9 +1,16 @@
 import { Typography } from "@mui/material";
 import { TimelineContainer } from "./TimelineStyled";
 
-export default function TimeLine() {
+interface TimeLineProps {
+  tweets: any[];
+  users: any[];
+}
+
+export default function TimeLine({ tweets, users }: TimeLineProps) {
   return (
     <TimelineContainer>
+      {tweets}
+      {users}
       <Typography variant="h1">TIMELINE</Typography>
     </TimelineContainer>
   );
